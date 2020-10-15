0e0d629a8f48fbb426bd02fa753345d73e71731d
Mathias Schmoigl <mathias.schmoigl@salzburgresearch.at>
2020-10-15 16:22:59 +0200

    preparation commit to enable device calls without GUI

--- devices/conveyorbelt/basys.conveyorbelt/src/main/java/org/srfg/MainConveyorBelt.java
+++ devices/conveyorbelt/basys.conveyorbelt/src/main/java/org/srfg/MainConveyorBelt.java
@@ -352,39 +352,48 @@ public class MainConveyorBelt extends javax.swing.JFrame {
      * @param args the command line arguments
      ********************************************************************************************************/
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainConveyorBelt.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainConveyorBelt.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainConveyorBelt.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainConveyorBelt.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainConveyorBelt().setVisible(true);
+        if(args.length == 1 && (args[0] == "noGUI")) // launch application without a GUI
+        {
+            System.out.println("Started application without GUI!\n Trying to host I4.0 Component:");
+            // TODO
+
+            System.out.println("\n Trying to register Belt:");
+            // TODO
+
+            System.out.println("\n Trying to start Belt communication:");
+            // TODO
+        }
+        else // launch application with a GUI
+        {
+            /* Set Nimbus look and feel (http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html) */
+            try {
+                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+                }
+            } catch (ClassNotFoundException ex) {
+                java.util.logging.Logger.getLogger(MainConveyorBelt.class
+                        .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                java.util.logging.Logger.getLogger(MainConveyorBelt.class
+                        .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                java.util.logging.Logger.getLogger(MainConveyorBelt.class
+                        .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+                java.util.logging.Logger.getLogger(MainConveyorBelt.class
+                        .getName()).log(java.util.logging.Level.SEVERE, null, ex);
             }
-        });
+
+            /* Create and display the form */
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    new MainConveyorBelt().setVisible(true);
+                }
+            });
+        }
     }
 }
--- devices/peak2pi/basys.Peak2Pi/src/main/java/org/srfg/MainPeak2PiDevice.java
+++ devices/peak2pi/basys.Peak2Pi/src/main/java/org/srfg/MainPeak2PiDevice.java
@@ -402,39 +402,48 @@ public class MainPeak2PiDevice extends javax.swing.JFrame {
      * @param args the command line arguments
      ********************************************************************************************************/
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainPeak2PiDevice.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainPeak2PiDevice.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainPeak2PiDevice.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainPeak2PiDevice.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainPeak2PiDevice().setVisible(true);
+        if(args.length == 1 && (args[0] == "noGUI")) // launch application without a GUI
+        {
+            System.out.println("Started application without GUI!\n Trying to host I4.0 Component:");
+            // TODO
+
+            System.out.println("\n Trying to register Peak2Pi:");
+            // TODO
+
+            System.out.println("\n Trying to start Peak2Pi communication:");
+            // TODO
+        }
+        else // launch application with a GUI
+        {
+            /* Set Nimbus look and feel (http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html) */
+            try {
+                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                    if ("Nimbus".equals(info.getName())) {
+                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+                }
+            } catch (ClassNotFoundException ex) {
+                java.util.logging.Logger.getLogger(MainPeak2PiDevice.class
+                        .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (InstantiationException ex) {
+                java.util.logging.Logger.getLogger(MainPeak2PiDevice.class
+                        .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                java.util.logging.Logger.getLogger(MainPeak2PiDevice.class
+                        .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+                java.util.logging.Logger.getLogger(MainPeak2PiDevice.class
+                        .getName()).log(java.util.logging.Level.SEVERE, null, ex);
             }
-        });
+
+            /* Create and display the form */
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    new MainPeak2PiDevice().setVisible(true);
+                }
+            });
+        }
     }
 }
