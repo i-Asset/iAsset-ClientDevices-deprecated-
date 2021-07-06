e0fe10ca9a4aefc3c0c7f8aeca33251c40af796f
fstroh <fstroh@gmail.com>
2021-07-06 12:40:27 +0200

    minor update in test.

--- devices/devices.panda/src/test/java/at/srfg/panda/nodes/ROSTest.java
+++ devices/devices.panda/src/test/java/at/srfg/panda/nodes/ROSTest.java
@@ -2,12 +2,12 @@ package at.srfg.panda.nodes;
 
 import franka_msgs.FrankaState;
 import org.junit.Test;
-import org.ros.message.MessageListener;
 import org.ros.namespace.GraphName;
 import org.ros.node.*;
 import org.ros.node.topic.Subscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sensor_msgs.JointState;
 
 import java.net.URI;
 
@@ -16,14 +16,14 @@ public class ROSTest {
 
     @Test
     public void rosCoreConnectionTest() {
-        URI masteruri = URI.create("http://192.168.48.41:11311");
+        URI masteruri = URI.create("http://il041:11311");
         NodeConfiguration subNodeConfiguration = NodeConfiguration.newPrivate(masteruri);
         NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
         MyTestNode testnode = new MyTestNode();
         nodeMainExecutor.execute(testnode, subNodeConfiguration);
         try {
-            logger.info("waiting 10 seconds for requests...");
-            Thread.sleep(10);
+            logger.info("listening for messages (5 seconds...");
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             logger.info("stopping system");
         }
@@ -40,16 +40,11 @@ public class ROSTest {
         }
 
         public void onStart(ConnectedNode connectedNode) {
-            final URI uri = connectedNode.lookupServiceUri("/franka_state_controller/franka_states");
-            logger.info("{}", uri);
-            logger.info("{}", connectedNode.getParameterTree());
-            Subscriber<FrankaState> subscriber = connectedNode.newSubscriber("/franka_state_controller/franka_states", FrankaState._TYPE);
-            subscriber.addMessageListener(new MessageListener<FrankaState>() {
-                @Override
-                public void onNewMessage(FrankaState message) {
-                    logger.info("I heard: {}", message);
-                }
-            });
+            Subscriber<FrankaState> frankaSub = connectedNode.newSubscriber("/franka_state_controller/franka_states", FrankaState._TYPE);
+            frankaSub.addMessageListener(message -> logger.info("/franka_state_controller/franka_states: {}", message.getOTEE()));
+
+            Subscriber<JointState> jointSub = connectedNode.newSubscriber("/joint_states", JointState._TYPE);
+            jointSub.addMessageListener(message -> logger.info("/joint_states: {}", message.getName()));
         }
 
     }
